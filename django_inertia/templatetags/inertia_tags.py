c90dd340c8aa2200761f7d84d1dcecb6290ae9d3
Peter DeVita <mewtwo2643@yahoo.com>
2022-07-05 16:31:17 -0400

    Fix potential issue with non-default INERTIA_PAGE_CONTEXT setting

--- django_inertia/templatetags/inertia_tags.py
+++ django_inertia/templatetags/inertia_tags.py
@@ -3,13 +3,14 @@ import json
 from django import template
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
+from django.conf import settings
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
 def inertia(context: template.Context, app_id: str = "app"):
-    page = context["page__"]
+    page = context[settings.INERTIA_PAGE_CONTEXT]
     return format_html(
         "<div id=\"{}\" data-page='{}'></div>", mark_safe(app_id), mark_safe(json.dumps(page))
     )
